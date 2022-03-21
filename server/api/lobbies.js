92e286263bbb3359c1c12c08541739d8c2efc5ec
Dan Lee <dan.jisang.lee@gmail.com>
2022-03-21 18:58:06 -0400

    tweaks to lobby api routes, added instance prototype method

--- server/api/lobbies.js
+++ server/api/lobbies.js
@@ -1,26 +1,55 @@
 const router = require('express').Router();
 const {
-  models: { Lobby },
+  models: { Lobby, User, Question },
 } = require('../db');
 module.exports = router;
-/**
-IN Database Model, add instance method to load questions
-Lobby.prototype.loadNewQuestions = async function(type = 'all'){
-  function getRandomInt(min, max) {
-    return Math.random() * (max-min) + min;
-  }
-  let questionList;
-  switch(type){
-    case 'easy': questionList = await Question.findAll({where: {category: easy}})
-    break;
-    case 'medium': questionList = await Question.findAll({where: {category: medium}})
-    break;
-    case 'hard': questionList = await Question.findAll({where: {category: hard}});
-    default: questionList = await Question.findAll();
-  }
-  this.setQuestions([getRandomInt(1, questionList.length), getRandomInt(1, questionList.length), getRandomInt(1, questionList.length), getRandomInt(1, questionList.length), getRandomInt(1, questionList.length)])
-}
-*/
+
+//IN Database Model, add instance method to load questions
+// Lobby.prototype.loadNewQuestions = async function (type = 'all') {
+//   let prevQuestions = [];
+//   let newQuestions = [];
+//   const currLobby = await Lobby.findOne({
+//     where: { name: this.name },
+//     include: { model: Question },
+//   });
+//   if (currLobby.questions.length > 0) {
+//     for (let i = 0; i < currLobby.questions.length; i++) {
+//       prevQuestions.push(currLobby.questions[i].id);
+//     }
+//   }
+//   function getRandomInt(min, max) {
+//     return Math.random() * (max - min) + min;
+//   }
+//   let questionList;
+//   switch (type) {
+//     case 'easy':
+//       questionList = await Question.findAll({ where: { category: easy } });
+//       break;
+//     case 'medium':
+//       questionList = await Question.findAll({ where: { category: medium } });
+//       break;
+//     case 'hard':
+//       questionList = await Question.findAll({ where: { category: hard } });
+//       break;
+//     default:
+//       questionList = await Question.findAll();
+//   }
+
+//   while (newQuestions.length < 5) {
+//     if (prevQuestions.length > 0) {
+//       const randomInt = getRandomInt(0, questionList.length - 1);
+//       if (
+// /        !prevQuestions.includes(questionList[randomInt].id) &&
+// /       !newQuestions.includes(questionList[randomInt].id)
+//       ) {
+//         newQuestions.push(questionList[randomInt].id);
+//       } else {
+//         continue;
+//       }
+//     }
+//   }
+//   this.setQuestions(newQuestions);
+// };
 
 /**
 * POST route to create a lobby, also generates a user as host
@@ -38,18 +67,43 @@ router.post('/', async (req, res, next) => {
       roundId: 1,
     });
     newLobby.loadNewQuestions('all');
-    const newUser = await newLobby.createUser({
+    await newLobby.createUser({
       name: username,
       isHost: true,
       correctPoints: 0,
       incorrectPoints: 0,
     });
-    res.json({ newLobby, newUser });
+    const lobbyWithUsers = await Lobby.findOne({
+      where: { name: lobbyName },
+      include: { model: User },
+    });
+    res.json(lobbyWithUsers);
   } catch (err) {
     next(err);
   }
 });
-
+//Route to create and add user to lobby - in lobbies routes because we will add a user and return the lobby with all users eager loaded
+router.post('/:lobbyName', async (req, res, next) => {
+  try {
+    const { lobbyName } = req.params;
+    const { username } = req.body;
+    const newUser = await User.create({
+      name: username,
+      isHost: false,
+      correctPoints: 0,
+      incorrectPoints: 0,
+    });
+    const currLobby = await Lobby.findOne({ where: { name: lobbyName } });
+    await currLobby.addUser(newUser);
+    const lobbyWithUsers = await Lobby.findOne({
+      where: { name: lobbyName },
+      include: { model: User },
+    });
+    res.json(lobbyWithUsers);
+  } catch (err) {
+    next(err);
+  }
+});
 //simple get request to get Lobby info, may not need to be used because of sockets
 router.get('/:lobbyName', async (req, res, next) => {
   try {
@@ -60,24 +114,30 @@ router.get('/:lobbyName', async (req, res, next) => {
     next(err);
   }
 });
-//Put request to update lobby roundID
+//Put request to update Lobby round id, and load new user scores
 router.put('/:lobbyName', async (req, res, next) => {
   try {
     const { lobbyName } = req.params;
     const { reset } = req.body;
     if (reset) {
-      const [updatedRows, updatedLobby] = await Lobby.update(
+      const [updatedRows, updatedLobbies] = await Lobby.update(
         { roundId: 1 },
         { where: { name: lobbyName }, returning: true }
       );
-      res.json(updatedLobby[0]);
+      // NEED TO LOAD NEW QUESTIONS ONTO LOBBY
+      updatedLobbies[0].loadNewQuestions('all');
+      const updatedLobby = await Lobby.findOne({
+        where: { name: lobbyName },
+        include: [{ model: User }, { model: Question }],
+      });
+      res.json(updatedLobby);
     } else {
-      // NOT SURE IF RETURNING WORKS ON INCREMENT, WILL NEED TO TEST
-      const updated = await Lobby.increment(
-        { roundId: 1 },
-        { where: { name: lobbyName }, returning: true }
-      );
-      res.json(updated);
+      await Lobby.increment({ roundId: 1 }, { where: { name: lobbyName } });
+      const updatedLobby = await Lobby.findOne({
+        where: { name: lobbyName },
+        include: [{ model: User }, { model: Question }],
+      });
+      res.json(updatedLobby);
     }
   } catch (err) {
     next(err);
