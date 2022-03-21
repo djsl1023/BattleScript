abf96b04c4c8dce4102c1324e075b8d0840991da
Dan Lee <dan.jisang.lee@gmail.com>
2022-03-21 14:59:49 -0400

    saved all removal changes

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import {Link} from 'react-router-dom'
-import {logout} from '../store'
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { logout } from '../store';
 
-const Navbar = () =>  {
-  const isLoggedIn = useSelector(state => !!state.auth.id)
-  const dispatch = useDispatch()
+const Navbar = () => {
+  const isLoggedIn = useSelector((state) => !!state.auth.id);
+  const dispatch = useDispatch();
 
   return (
     <div>
@@ -15,7 +15,7 @@ const Navbar = () =>  {
           <div>
             {/* The navbar will show these links after you log in */}
             <Link to="/home">Home</Link>
-            <a href="#" onClick={ () => dispatch(logout()) }>
+            <a href="#" onClick={() => dispatch(logout())}>
               Logout
             </a>
           </div>
@@ -29,7 +29,7 @@ const Navbar = () =>  {
       </nav>
       <hr />
     </div>
-    )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
