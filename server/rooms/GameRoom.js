5c7554124880acc0d05d74fe429691c84a37842a
Dan Lee <dan.jisang.lee@gmail.com>
2022-03-23 18:19:51 -0400

    trying to get colyseus schemas working

--- server/rooms/GameRoom.js
+++ server/rooms/GameRoom.js
@@ -1,9 +1,63 @@
 const colyseus = require('colyseus');
+const schema = require('@colyseus/schema');
+const Schema = schema.Schema;
+const ArraySchema = schema.ArraySchema;
+const {
+  models: { Lobby, User, Question },
+} = require('../db');
+
+// INDIVIDUAL STATES THAT MAKE UP GAME STATE
+class UserSchema extends Schema {
+  constructor() {
+    super();
+    this.username = '';
+  }
+}
+
+class QuestionSchema extends Schema {
+  constructor() {
+    super();
+    this.difficulty = false;
+    this.title = '';
+    this.question = '';
+    this.testSpecs = '';
+  }
+}
+schema.defineTypes(QuestionSchema, {
+  difficulty: 'string',
+  title: 'string',
+  question: 'string',
+  testSpecs: 'string',
+});
+
+// OVERALL GAME STATE
+class GameState extends Schema {
+  constructor() {
+    super();
+    this.questions = new ArraySchema();
+  }
+}
+schema.defineTypes(GameState, {
+  questions: [QuestionSchema],
+});
 
 class GameRoom extends colyseus.Room {
   // When room is initialized
-  onCreate(options) {
+  async onCreate(options) {
+    this.setState(new GameState());
+
+    console.log('hello', this.state.GameState);
+    const abcd = this.questions;
     console.log('Room Created');
+    const questionList = await Question.findAll();
+    questionList.forEach((question, i) => {
+      const temp = new QuestionSchema();
+      temp.difficulty = question.difficulty;
+      temp.title = question.title;
+      temp.question = question.question;
+      temp.testSpecs = question.testSpecs;
+      abcd.push(temp);
+    });
   }
 
   // Authorize client based on provided options before WebSocket handshake is complete
