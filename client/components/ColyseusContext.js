44bbb8b047d4b42cd585198ca9e9e020d96b197b
Dan Lee <dan.jisang.lee@gmail.com>
2022-03-28 18:59:07 -0400

    heroku setup for github automatic integration

--- client/components/ColyseusContext.js
+++ client/components/ColyseusContext.js
@@ -1,12 +1,10 @@
 import React, { useContext, createContext } from 'react';
 import * as Colyseus from 'colyseus.js';
 
-// for testing on local host
-let client = new Colyseus.Client('ws://localhost:8080');
-
-// for deployment to heroku
-// let client = new Colyseus.Client('ws://fsa-battlescript.herokuapp.com/');
-
+// let client = new Colyseus.Client('ws://localhost:8080');
+let client = new Colyseus.Client(
+  'https://capstone-battlescript.herokuapp.com/'
+);
 export const ColyseusContext = createContext(client);
 
 export function useColyseus() {
