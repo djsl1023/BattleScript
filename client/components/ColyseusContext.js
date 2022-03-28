a714c0b7a52625b14a4d03af5d3f62efa53979e8
Dan Lee <dan.jisang.lee@gmail.com>
2022-03-28 19:01:33 -0400

    ws connection

--- client/components/ColyseusContext.js
+++ client/components/ColyseusContext.js
@@ -2,9 +2,7 @@ import React, { useContext, createContext } from 'react';
 import * as Colyseus from 'colyseus.js';
 
 // let client = new Colyseus.Client('ws://localhost:8080');
-let client = new Colyseus.Client(
-  'https://capstone-battlescript.herokuapp.com/'
-);
+let client = new Colyseus.Client('ws://capstone-battlescript.herokuapp.com/');
 export const ColyseusContext = createContext(client);
 
 export function useColyseus() {
