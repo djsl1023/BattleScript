049ea34cb90cb681cb7d3b1e2dd423474c3368c7
Dan Lee <dan.jisang.lee@gmail.com>
2022-03-28 02:50:54 -0400

    voting page still needs work on client,server logic

--- client/components/Vote.js
+++ client/components/Vote.js
@@ -1,17 +1,35 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Editor from '@monaco-editor/react';
 import { useSelector, useDispatch } from 'react-redux';
+import { setFailedVotes } from '../store/failVoting';
+import { setPassedVotes } from '../store/passVoting';
 
 const Vote = () => {
   const editorRef = useRef(null);
   const dispatch = useDispatch();
+  const [voted, setVoted] = useState(false);
+  const [votedFor, setVotedFor] = useState('');
   const [userFocus, setUserFocus] = useState('');
   const users = useSelector((state) => state.users);
+  const room = useSelector((state) => state.room);
   const currPrompt = useSelector((state) => state.prompt);
+  const voteList = useSelector((state) => {
+    if (state.gameStatus === 'failvote') {
+      return state.failedVotes;
+    } else if (state.gameStatus === 'passvote') {
+      return state.passedVotes;
+    }
+  });
   const submissions = useSelector((state) => {
     if (state.gameStatus === 'failvote') {
+      state.failVotes.onChange = (clientVotes, key) => {
+        dispatch(setFailedVotes(key, clientVotes));
+      };
       return state.failedAnswers;
     } else if (state.gameStatus === 'passvote') {
+      state.passVotes.onChange = (clientVotes, key) => {
+        dispatch(setPassedVotes(key, clientVotes));
+      };
       return state.passedAnswers;
     }
   });
@@ -24,8 +42,14 @@ const Vote = () => {
     setUserFocus(userKey);
     editorRef.current.setValue(submissions[userKey].answer);
   };
+
+  const voteHandler = (e, userFocus, gameStatus) => {
+    setVoted(true);
+    setVotedFor(users[userFocus].username);
+    room.send(gameStatus, { solutionId: userFocus });
+  };
   return (
-    <div>
+    <div className="prompt-container">
       <div className="prompt-solution">
         <Editor
           height="95vh"
@@ -50,13 +74,19 @@ const Vote = () => {
           }}
           onMount={handleEditorDidMount}
         />
+        <button
+          className="submit-prompt-button"
+          onClick={(e) => voteHandler(e, userFocus, room.state.gameStatus)}
+          disabled={voted}>
+          {voted ? `You've already voted for ${votedFor}!` : 'Vote!'}
+        </button>
       </div>
       <div className="user-list">
         {Object.keys(submissions).map((userKey) => {
           return (
             <div key={userKey}>
               <button value={userKey} onClick={(e) => changeFocus(e, userKey)}>
-                {users[userKey].username}
+                {users[userKey].username}: {}
               </button>
             </div>
           );
