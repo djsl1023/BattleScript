ad60b60597d9d5ad9c7f3e1263fd5a56d0c0d162
Matthew Carroll <mjcarroll777@gmail.com>
2022-03-29 20:45:26 -0400

    Altered webpack to accomidate CSS modueles

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import Rules from './Rules';
 import { userLeaveRoom } from '../store';
 import { useDispatch } from 'react-redux';
+import styles from '../styles/Navbar.module.css';
 
 const Navbar = () => {
   const [isShown, setShown] = useState(false);
@@ -13,30 +14,33 @@ const Navbar = () => {
   return (
     <div>
       <nav>
-        <div className="navbar">
+        {console.log(styles)}
+        {console.log(styles.navbar)}
+        <div className={styles.navbar}>
           <Link to="/" onClick={handleClick}>
-            <li className="nav-list">Home </li>
+            <li className={styles.navList}> Home </li>
           </Link>
-          <li className="nav-list">
+          <li className={styles.navList}>
             <button
-              className="rules-btn"
+              className={styles.rulesBtn}
               onMouseEnter={() => setShown(true)}
-              onMouseLeave={() => setShown(false)}>
+              onMouseLeave={() => setShown(false)}
+            >
               Rules
             </button>
             {isShown && (
-              <div id="rules-comp">
+              <div id={styles.rulesComp}>
                 <Rules />
               </div>
             )}
           </li>
-          <li className="nav-list" id="center-logo">
+          <li className={styles.navList} id={styles.centerLogo}>
             BATTLESCRIPT
           </li>
-          <li className="nav-list" id="github-logo">
+          <li className={styles.navList} id={styles.githubLogo}>
             <a href="https://github.com/FSCapstone/Capstone_Team_14_JS">
               <img
-                id="github-logo"
+                id={styles.githubLogo}
                 src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
               />
             </a>
