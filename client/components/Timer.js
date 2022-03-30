5b2eb908de59e341a532651d2d91b833abeeef7d
Hannah Gassmann <94023194+hgassmann91@users.noreply.github.com>
2022-03-29 22:27:42 -0500

    timer clean up

--- client/components/Timer.js
+++ client/components/Timer.js
@@ -11,19 +11,36 @@ const Timer = () => {
   let completed = ((150 - timer) / 150) * 100;
   console.log(timer);
   const containerStyles = {
+    display: 'flex',
     height: 20,
     width: '100%',
+    paddingLeft: '1em',
+    //white
     backgroundColor: '#e0e0de',
+    animation: `animate-stripes 0.6s linear infinite`,
     borderRadius: 50,
-    margin: 50,
+    // margin: 50,
+    marginTop: 50,
+    marginBottom: 50,
+    marginleft: 20,
+    // padding: 10,
+
+    //     margin: '100px auto',
+    // marginTop: '220px',
+    // width: '400px',
+    // textAlign: 'center',
+    // position: 'relative',
   };
 
   const fillerStyles = {
     height: '100%',
     width: `${completed}%`,
+    //contrast
     backgroundColor: '#D17659',
     borderRadius: 'inherit',
     textAlign: 'right',
+    // paddingLeft: 10,
+    // animation: `progress ${timer}s`,
   };
 
   const labelStyles = {
@@ -35,8 +52,10 @@ const Timer = () => {
   return (
     <div>
       <div style={containerStyles}>
-        <div style={fillerStyles}>
-          <span style={labelStyles}>{timer}</span>
+        <div className="fillerStyles" style={fillerStyles}>
+          <span style={labelStyles}>{`${Math.floor(timer / 60)}:${String(
+            Math.ceil(timer % 60)
+          ).padStart(2, '0')}`}</span>
         </div>
       </div>
     </div>
