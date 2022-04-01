e63b91b16a658d75a0868a682fbc2ac76de10e3a
Matthew Carroll <mjcarroll777@gmail.com>
2022-04-01 17:52:00 -0400

    formatted lobby page to fit avatars

--- client/components/Lobby.js
+++ client/components/Lobby.js
@@ -11,55 +11,15 @@ const Lobby = () => {
   return (
     <div>
       <h4> Lobby: room Id : {room.id} </h4>
-      {/* <div className={styles.lobbyWrapper}>
-        {['cody', 'chris', 'mike', 'sam', 'liam', 'ryan', 'smith', 'chloe'].map(
-          (clientId) => {
-            return (
-              <div>
-                {clientId} <img src="./Images/Avatars/doge.png" />{' '}
-              </div>
-            );
-          }
-        )}
-      </div> */}
-      <div className="lobby-container">
-        <div className="table">
-          <div id="abv-table">
-            {Object.keys(users).map((clientId, idx) => {
-              if (idx < 3)
-                return (
-                  <li key={clientId} className="seat">
-                    <p>{users[clientId].username}</p>
-                    <img
-                      src={users[clientId].avatarURL}
-                      className="player-avatar"
-                    />
-                  </li>
-                );
-            })}
-          </div>
-          <img
-            id="table-img"
-            src={
-              'https://betandbeat.com/wp-content/uploads/2021/03/oval-poker-table.svg'
-            }></img>
-          <div id="below-table">
-            {Object.keys(users).map((clientId, idx) => {
-              if (idx >= 3)
-                return (
-                  <li key={clientId} className="seat">
-                    <p>{users[clientId].username}</p>
-                    <img
-                      src={
-                        'https://as1.ftcdn.net/v2/jpg/03/53/11/00/1000_F_353110097_nbpmfn9iHlxef4EDIhXB1tdTD0lcWhG9.jpg'
-                      }
-                      className="player-avatar"
-                    />
-                  </li>
-                );
-            })}
-          </div>
-        </div>
+      <div className={styles.lobbyWrapper}>
+        {Object.keys(users).map((clientId) => {
+          return (
+            <div className={styles.element} key={clientId}>
+              <img src={users[clientId].avatarURL} className={styles.avatar} />
+              <div className={styles.name}>{users[clientId].username}</div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
