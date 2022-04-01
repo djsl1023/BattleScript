29bd11b2d1415f557db4fccc586da3131353e474
Matthew Carroll <mjcarroll777@gmail.com>
2022-03-31 21:30:51 -0400

    Seeded new easy questions; styled messager component

--- client/components/ColyseusContext.js
+++ client/components/ColyseusContext.js
@@ -10,5 +10,9 @@ export function useColyseus() {
 }
 
 export default function ColyseusProvider(props) {
-  return <ColyseusContext.Provider value={client}>{props.children}</ColyseusContext.Provider>;
+  return (
+    <ColyseusContext.Provider value={client}>
+      {props.children}
+    </ColyseusContext.Provider>
+  );
 }
