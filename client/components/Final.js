aa6b87849dab60c88920ec345cbdae7499a0ce0d
Jonathan Donadio <jondonadio@gmail.com>
2022-04-01 16:32:30 -0400

    added css for final component

--- client/components/Final.js
+++ client/components/Final.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import styles from '../styles/Final.module.css';
+
+const dummyUser = {
+  total: 5000,
+  username: 'asdjoaomdo',
+};
 
 const Final = () => {
   const users = useSelector((state) => state.users);
@@ -27,14 +33,26 @@ const Final = () => {
   const winner = findHighestScore(clientIds);
 
   return (
-    <div>
-      <div>Winner: {users[winner].username}</div>
-      <div>
-        Total Score:{' '}
-        {users[winner].correctPoints + users[winner].incorrectPoints}
+    <div className={styles.finalContainer}>
+      <div className={styles.header}>
+        <h1>The Winner Is....</h1>
+      </div>
+
+      <div className={styles.playerContainer}>
+        <div className={styles.avatar}>
+          <img src="./Images/Avatars/doge.png" />
+        </div>
+        <p>{dummyUser.username}</p>
+
+        <p>Total Points: {dummyUser.total}</p>
       </div>
     </div>
   );
 };
 
 export default Final;
+
+{
+  /* <div>Winner: {users[winner].username}</div>
+      <div>Total Score: {users[winner].correctPoints + users[winner].incorrectPoints}</div> */
+}
