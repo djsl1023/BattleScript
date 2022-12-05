1253358b8ca72c1f1cacd82202a0246496bf6797
djsl1023 <91397108+djsl1023@users.noreply.github.com>
2022-12-05 17:01:59 -0500

    Update ColyseusContext.js

--- client/components/ColyseusContext.js
+++ client/components/ColyseusContext.js
@@ -2,7 +2,7 @@ import React, { useContext, createContext } from 'react';
 import * as Colyseus from 'colyseus.js';
 
 // let client = new Colyseus.Client('ws://localhost:8080');
-let client = new Colyseus.Client('ws://capstone-battlescript.herokuapp.com/');
+let client = new Colyseus.Client('ws://battlescript.onrender.com/');
 export const ColyseusContext = createContext(client);
 
 export function useColyseus() {
